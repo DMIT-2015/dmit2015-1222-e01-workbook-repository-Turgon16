6853ca7a4ba24c49a3080b9bc194509a3fcfa766
Tyler Hum <thum2@nait.ca>
2023-01-13 20:49:01 -0700

    Practicing Java syntax for classes

---
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/Circle.java
@@ -0,0 +1,94 @@
+package dmit2015.model;
+
+public class Circle {
+// fields
+    private double radius;
+// getter
+    public double getRadius() {
+        return radius;
+    }
+// setter
+    //CHECKED exception example
+    public void setRadius(double radius) throws Exception {
+        // let's do exception testing
+        // Validate the new value assigned to the  Radius
+        if (radius <= 0)
+        {
+            throw new Exception("Radius must be a positive non-zero number.");
+        }
+        this.radius = radius;
+    }
+
+    // Runtime exception Example
+//    public void setRadius(double radius) throws Exception {
+//        // let's do exception testing
+//        // Validate the new value assigned to the  Radius
+//        if (radius <= 0)
+//        {
+//            throw new RuntimeException("Radius must be a positive non-zero number.");
+//        }
+//        this.radius = radius;
+//    }
+// empty constructor
+    public Circle() {
+        this.radius = 1;
+    }
+// constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+// Methods
+    public double Area()
+    {
+        return Math.PI * radius * radius;
+    }
+    public double Perimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+// class with main is a program, class without main is not a program
+    public static void main(String[] args) {
+        // create new circle object
+        Circle circle1 = new Circle();
+        // the radius of circle1 should be 1,
+        // area should be 3.14159
+        // perimeter should be 6.28
+        // string formatting is a bit different than C#
+        // s formats strings, d for integers, f for floating point numbers, d for dates and times
+        // System.out.print is like Console.writeline in C#
+        System.out.println("The radius of circle1 is " + circle1.getRadius());
+        System.out.printf("The radius of circle1 is %s\n", circle1.getRadius());
+        System.out.printf("The area of circle1 is %.5f\n", circle1.Area());
+        System.out.printf("The perimeter of circle1 is %.2f", circle1.Perimeter());
+
+        // Change the radius of circle 1 to 5
+        //
+        try
+        {
+            circle1.setRadius(5);
+            // the radius of circle1 should be 5,
+            // area should be 78.53982
+            // perimeter should be 31.41593
+            System.out.printf("The radius of circle1 is %s\n", circle1.getRadius());
+            System.out.printf("The area of circle1 is %.5f\n", circle1.Area());
+            System.out.printf("The perimeter of circle1 is %.5f", circle1.Perimeter());
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+        // Change the radius of circle1 to -25
+        // It should throw an exception
+        try
+        {
+            circle1.setRadius(-25);
+            System.out.println("An exception should have been thrown");
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+}
